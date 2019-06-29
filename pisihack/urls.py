3587a80d0d14725a74d169f4033aff48aed59646
cherenkov <tonegrep@gmail.com>
2019-06-29 22:44:06 +0300

    some template and view improvements

--- pisihack/urls.py
+++ pisihack/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
-
+    path('profile/', include('user_profile.urls'))
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class ProfileView(TemplateView):
+    
+
+
 
 # Create your views here.
