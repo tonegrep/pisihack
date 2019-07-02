26638528ca36a118eabec7edba8bd3229852a233
ontrigger <firewarpcd@gmail.com>
2019-07-03 02:14:44 +0300

    update project to directly use the user model

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import requests
-from django.contrib.auth.models import AbstractUser
-
-from allauth.socialaccount.signals import pre_social_login
-
-
-class PisiUser(AbstractUser):
-    pass
